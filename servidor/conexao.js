0d859720a96ae05083a700bf69fb1e5d8ac96c59
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-07-08 12:36:41 -0300

    banco ajustado aos requisitos

--- servidor/conexao.js
+++ servidor/conexao.js
@@ -5,6 +5,19 @@ const sequelize = new Sequelize('scfv', 'root', 'admin', {
 })
 
 // constantes para os modelos que devem virar tabelas
+const Contato = sequelize.define('contato', {
+    codigo: {
+        type: Sequelize.INTEGER
+    },
+    contato: {
+        type: Sequelize.STRING
+    },
+    tipo: {
+        type: Sequelize.INTEGER
+    }
+})
+
+
 const Escola = sequelize.define('escola', {
     codigo: {
         type: Sequelize.INTEGER
@@ -29,6 +42,30 @@ const Colaborador = sequelize.define('colaborador', {
     },
     datanasc: {
         type: Sequelize.DATE
+    },
+    codContato: {
+        type: Sequelize.STRING
+    }
+})
+
+const Responsavel = sequelize.define('responsavel', {
+    codigo: {
+        type: Sequelize.INTEGER
+    },
+    codigoUsuario: {
+        type: Sequelize.INTEGER
+    },
+    nome: {
+        type: Sequelize.STRING
+    },
+    bairro: {
+        type: Sequelize.STRING
+    },
+    datanasc: {
+        type: Sequelize.DATE
+    },
+    codContato: {
+        type: Sequelize.STRING
     }
 })
 
@@ -57,9 +94,11 @@ const Usuario = sequelize.define('usuario', {
 })
 
     // Para criar uma tabela o banco referente ao modelo
-    // Usuario.sync({force: true});
-    // Escola.sync({force: true});
-    // Colaborador.sync({force: true});
+    Usuario.sync({force: true});
+    Escola.sync({force: true});
+    Contato.sync({force: true});
+    Colaborador.sync({force: true});
+    Responsavel.sync({force: true});
 
         // // Para inserir um registro na tabela
         // Usuario.create({
