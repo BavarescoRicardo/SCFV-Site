23cad52136786048668c64e4323acbd12e70a330
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-07-13 00:20:38 -0300

    models presenca

---
+++ servidor/models/Presenca.js
@@ -0,0 +1,24 @@
+const conexao = require('./conexao')
+
+// constantes para os modelos que devem virar tabelas
+const Presenca = conexao.sequelize.define('contato', {
+    codigo: {
+        type: conexao.Sequelize.INTEGER
+    },
+    dia: {
+        type: conexao.Sequelize.DATE
+    },
+    turma: {
+        type: conexao.Sequelize.INTEGER
+    },
+    oficina: {
+        type: conexao.Sequelize.INTEGER
+    },
+    turno: {
+        type: conexao.Sequelize.INTEGER
+    }
+})
+
+// Presenca.sync({force: true});
+
+module.exports = Presenca
