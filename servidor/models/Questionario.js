ad5351b7a5acec8d2a89052b14b7f83eab8dfab8
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-07-31 17:12:17 -0300

    models questionario

---
+++ servidor/models/Questionario.js
@@ -0,0 +1,21 @@
+const conexao = require('./conexao')
+
+// constantes para os modelos que devem virar tabelas
+const Questionario = conexao.sequelize.define('questionario', {
+    codigo: {
+        type: conexao.Sequelize.INTEGER
+    },
+    codigoProfessorLogado: {
+        type: conexao.Sequelize.INTEGER
+    },
+    codigoPresenca: {
+        type: conexao.Sequelize.INTEGER
+    },
+    questao: {
+        type: conexao.Sequelize.TEXT
+    }
+})
+
+// Questionario.sync({force: true});
+
+module.exports = Presenca
---
+++ servidor/models/UsuarioRespostaQuestionario.js
@@ -0,0 +1,21 @@
+const conexao = require('./conexao')
+
+// constantes para os modelos que devem virar tabelas
+const UsuarioRespostaQuestionario = conexao.sequelize.define('questionario', {
+    codigo: {
+        type: conexao.Sequelize.INTEGER
+    },
+    codigoUsuario: {
+        type: conexao.Sequelize.INTEGER
+    },
+    codigoPresenca: {
+        type: conexao.Sequelize.INTEGER
+    },
+    resposta: {
+        type: conexao.Sequelize.TEXT
+    }
+})
+
+// UsuarioRespostaQuestionario.sync({force: true});
+
+module.exports = Presenca
